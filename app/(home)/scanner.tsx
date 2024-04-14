be6c5a266f8cbf3a9a95110be2faa9c71481f3ca
Ibrahima Gueye <ibrahimagueye@Ibrahimas-Mac-mini.local>
2024-04-14 01:27:43 -0400

    scanner

--- app/(home)/scanner.tsx
+++ app/(home)/scanner.tsx
@@ -1,18 +1,29 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 export default function ScannerScreen() {
+  const [scanned, setScanned] = useState(false);
+
+  const handleBarCodeRead = (e: { type: any; data: any; }) => {
+    setScanned(true);
+    Alert.alert('Barcode Found!', `Type: ${e.type}\nData: ${e.data}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Scanner</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(home)/scanner.tsx" />
+      <Text style={styles.title}>Scan your Barcode</Text>
+      <RNCamera
+        style={styles.preview}
+        onBarCodeRead={scanned ? undefined : handleBarCodeRead}
+        type={RNCamera.Constants.Type.back}
+        captureAudio={false}
+        barCodeTypes={[RNCamera.Constants.BarCodeType.qr, RNCamera.Constants.BarCodeType.code128]} // You can specify types here
+      >
+        {scanned && (
+          <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+        )}
+      </RNCamera>
     </View>
   );
 }
@@ -20,16 +31,19 @@ export default function ScannerScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    padding: 20,
   },
 });
